12208422c56ec806659b177f104db7395b89bf7e
hoangvh238 <hoangvh238.dev@gmail.com>
2023-12-20 16:26:19 +0700

    update base

---
+++ GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using Ardalis.GuardClauses;
+global using dailyquiz.Web.Infrastructure;
+global using MediatR;
---
+++ Services/CurrentUser.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+using dailyquiz.Application.Common.Interfaces;
+
+namespace dailyquiz.Web.Services;
+
+public class CurrentUser : IUser
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentUser(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public string? Id => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+}
